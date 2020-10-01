a0d4541423ca02c9dc8234fb317b70b09de3188a
romy421kumari <44578093+romy421kumari@users.noreply.github.com>
2020-10-01 10:57:25 +0530

    Create infix2pefix.py

---
+++ infix2pefix.py
@@ -0,0 +1,87 @@
+class Conversion: 
+    operator=['and','or','(',')']  
+    # Constructor to initialize the class variables 
+    def __init__(self): 
+        self.top = -1 
+        
+        self.array = [] 
+        # Precedence setting 
+        self.output = [] 
+        
+      
+    # check if the stack is empty 
+    def isEmpty(self): 
+        return True if self.top == -1 else False
+      
+    # Return the value of the top of the stack 
+    def peek(self): 
+        return self.array[-1] 
+      
+    # Pop the element from the stack 
+    def pop(self): 
+        if not self.isEmpty(): 
+            self.top -= 1
+            return self.array.pop() 
+        else: 
+            return "$"
+      
+    # Push the element to the stack 
+    def push(self, op): 
+        self.top += 1
+        self.array.append(op)  
+  
+    # A utility function to check is the given character 
+    # is operand  
+    def isOperand(self, ch): 
+        if ch not in operator:
+            return ch
+  
+   
+    # The main function that converts given infix expression 
+    # to postfix expression 
+    def infixToPostfix(self, exp): 
+          
+        # Iterate over the expression for conversion 
+        for i in exp: 
+            # If the character is an operand,  
+            # add it to output 
+            if self.isOperand(i): 
+                self.output.append(i) 
+              
+            # If the character is an '(', push it to stack 
+            elif i  == '(': 
+                self.push(i) 
+  
+            # If the scanned character is an ')', pop and  
+            # output from the stack until and '(' is found 
+            elif i == ')': 
+                while( (not self.isEmpty()) and self.peek() != '('): 
+                    a = self.pop() 
+                    self.output.append(a) 
+                if (not self.isEmpty() and self.peek() != '('): 
+                    return -1
+                else: 
+                    self.pop() 
+  
+            # An operator is encountered 
+            else: 
+                while(not self.isEmpty()): 
+                    self.output.append(self.pop()) 
+                self.push(i) 
+  
+        # pop all the operator from the stack 
+        while not self.isEmpty(): 
+            self.output.append(self.pop()) 
+  
+        print "".join(self.output) 
+  
+# Driver program to test above function 
+exp = "a+b*(c^d-e)^(f+g*h)-i"
+obj = Conversion(len(exp)) 
+obj.infixToPostfix(exp) 
+  
+
+
+
+
+
